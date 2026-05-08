018bcf5ac7e4ce83b951f1a0d26ba0e6f9eda167
abhishek <abhi1999@gmail.com>
2026-05-08 11:11:00 -0400

    add fav icon

--- sheets-editor/src/app/layout.tsx
+++ sheets-editor/src/app/layout.tsx
@@ -5,6 +5,13 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'NAYCA 2026 Scheduling Sheet',
   description: 'Securely view and edit NAYCA 2026 Scheduling Google Sheets.',
+  icons: [
+    {
+      rel: 'icon',
+      url: '/favicon.png',
+      type: 'image/png',
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -17,6 +24,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=DM+Mono:wght@400;500&display=swap"
           rel="stylesheet"
